93705c3782ce3868e7b875817820e1f651cbe4b9
tanviriqbal <tanviriqbal@CEL52>
2019-09-12 09:10:07 +0600

    Initial Commit

---
+++ DemoLibrary/ShoppingCartModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class ShoppingCartModel
+    {
+        public List<ProductModel> Items { get; set; } = new List<ProductModel>();
+        
+        public decimal GenerateTotal()
+        {
+            decimal subTotal = Items.Sum(x => x.Price);
+            if (subTotal > 100)
+            {
+                return subTotal * 0.80M;
+            }
+            else if (subTotal > 50)
+            {
+                return subTotal * 0.85M;
+            }
+            else if (subTotal > 10)
+            {
+                return subTotal * 0.90M;
+            }
+            else
+            {
+                return subTotal;
+            }
+        }
+    }
+}
